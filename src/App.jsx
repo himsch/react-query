7a7feead4f4b92ae6d6382bd8f94f9018312086d
song <55rsearchco@gmail.com>
2023-02-28 10:27:20 +0900

    리액트 쿼리 사용해보기, useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css';
 import MainProducts from './components/MainProducts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <MainProducts />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainProducts />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react';
 import useProducts from '../hooks/use-products';
+import { useQuery } from '@tanstack/react-query';
 
 export default function Products() {
   const [checked, setChecked] = useState(false);
-  const [loading, error, products] = useProducts({ salesOnly: checked });
+  const {
+    isLoading,
+    error,
+    data: products,
+  } = useQuery(['products'], async () => {
+    console.log('fetching..........');
+    const res = await fetch(`data/products.json`);
+    return await res.json();
+  });
+  // const [loading, error, products] = useProducts({ salesOnly: checked });
   const handleChange = () => setChecked(prev => !prev);
 
-  if (loading) return <p>Loading</p>;
+  if (isLoading) return <p>Loading</p>;
 
   if (error) return <p>{error}</p>;
 
